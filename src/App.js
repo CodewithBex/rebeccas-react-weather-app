bd718a7055b84fabe7f5d0fa9f30f72989926b3e
CodewithBex <73992265+CodewithBex@users.noreply.github.com>
2021-05-21 13:31:45 +0100

    added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -3,7 +3,18 @@ import './App.css';
 
 export default function App() {
   return (
-    <div className="App">Weather App</div>
+    <div className="App">
+      <h1>
+        Weather App
+            </h1>
+      <footer>
+This project was coded by <a href="https://www.linkedin.com/in/rebecca-mary-kanaan-1a08a459/" target="_blank">Rebecca Mary K.</a> and is {""}
+      <a href="https://github.com/CodewithBex/rebeccas-react-weather-app"
+        target="_blank">
+         open-sourced on GitHub
+        </a>
+          </footer>
+    </div>
   );
 }
 
